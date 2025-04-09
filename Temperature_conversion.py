7006d4ddf5ac0d2c5b58c8f1b3dcfee9d21f763b
Dua Eman <duaeman.726@gmail.com>
2025-04-09 15:54:03 +0500

    Add files via upload
    here is the code to convert the Celsius temperature into Fahrenheit.

---
+++ Temperature_conversion.py
@@ -0,0 +1,3 @@
+temp_in_celsius = float(input("enter the temperature in celsius"))
+temp_in_Fahrenheit = float((temp_in_celsius * 9/5) + 32)
+print("the temperature in fahrenhite is: ", temp_in_Fahrenheit)
