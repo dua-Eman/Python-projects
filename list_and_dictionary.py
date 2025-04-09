a54838703053ec87068f70b5d3ee055c6da272d6
Dua Eman <duaeman.726@gmail.com>
2025-04-09 16:01:29 +0500

    Add files via upload
    here is the use of list and dictionary in this code.

---
+++ list_and_dictionary.py
@@ -0,0 +1,11 @@
+# list
+fvrt_movies=["train to busan", "20th century girl", "wednesday"]
+print("my favourite movies are : ", fvrt_movies)
+
+# dictionary
+person = {
+    "name": "Dua Eman",
+    "age": 22,
+    "email": "dua@gmail.com"
+ }
+print("information", person)
